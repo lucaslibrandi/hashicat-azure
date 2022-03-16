e1268b3a0eccd2277f85308c18d431109f0f6a98
lucaslibrandi <40200525+lucaslibrandi@users.noreply.github.com>
2022-03-16 16:34:24 +1300

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/hashilab/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "labRG"
+  resource_group_name = "${var.prefix}-workshop"
 }
